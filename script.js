205442dab118a0faf8afd6d1e9761f47a80ec717
TinekeKvdW <84176956+TinekeKvdW@users.noreply.github.com>
2021-06-07 14:03:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let person = {
+    name: "Tineke",
+    age: 36,
+    evaluations: [7, 10, 9],
+};
+
+let selectedColors = ["geel", "blauw", "rood"];
+const count = selectedColors.push("groen");
+const total = selectedColors.push("4");
+let object = {
+    greeting: "hoi, ik ben een object."
+};
+
+console.log(person.name);
+console.log(person.age);
+console.log(selectedColors);
+console.log(selectedColors.length);
+console.log(selectedColors[0]);
+console.log(object);
+
+
+
+/*console.log(selectedColors.????????????? voor de laatste kleur??
+    en ook evaluations lukt niet.
+    */
